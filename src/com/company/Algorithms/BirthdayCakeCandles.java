03e3d64a07456f8eb25190a9baac48014a716756
Simonas D <dranxman@gmail.com>
2018-11-18 14:06:59 +0200

    finish cake candles problem

--- src/com/company/Algorithms/BirthdayCakeCandles.java
+++ src/com/company/Algorithms/BirthdayCakeCandles.java
@@ -1,7 +1,24 @@
 package com.company.Algorithms;
 
 public class BirthdayCakeCandles {
-    public static void main(String[] args) {
+    static int birthdayCakeCandles(int[] ar) {
+        int tallest = 0;
+        int frequency = 0;
 
+        for (int i = 0; i < ar.length; i++) {
+            int height = ar[i];
+
+            if (height > tallest) {
+                tallest = height;
+                frequency = 1;
+            }
+            else if (height == tallest) frequency++;
+        }
+        return frequency;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 3};
+        System.out.println(birthdayCakeCandles(arr));
     }
 }
