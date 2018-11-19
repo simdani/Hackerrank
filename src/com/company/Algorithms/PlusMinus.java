4af4f5373d11b50f13a719e095d85aa85dff47ae
Simonas D <dranxman@gmail.com>
2018-11-19 18:08:53 +0200

    solve plus minus problem

---
+++ src/com/company/Algorithms/PlusMinus.java
@@ -0,0 +1,45 @@
+package com.company.Algorithms;
+
+public class PlusMinus {
+    static void plusMinus(int[] arr) {
+        float positive = 0;
+        float negative = 0;
+        float zeros = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                positive++;
+            } else if (arr[i] == 0) {
+                zeros++;
+            } else {
+                negative++;
+            }
+        }
+
+        if (positive == 0) {
+            System.out.println(positive);
+        }
+        else {
+            System.out.println(positive / arr.length);
+        }
+
+        if (negative == 0) {
+            System.out.println(negative);
+        }
+        else {
+            System.out.println(negative / arr.length);
+        }
+
+        if (zeros == 0) {
+            System.out.println(zeros);
+        }
+        else {
+            System.out.println(zeros / arr.length);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {-4, 3, -9, 0, 4, 1};
+        plusMinus(arr);
+    }
+}
