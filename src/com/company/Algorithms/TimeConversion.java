24c1a56827bc3196cd19fa33aa6adc63ba82490e
Simonas D <dranxman@gmail.com>
2018-11-20 21:15:58 +0200

    add time conversion algorithm

---
+++ src/com/company/Algorithms/TimeConversion.java
@@ -0,0 +1,25 @@
+package com.company.Algorithms;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeConversion {
+    static String TimeConversion(String s) {
+        try {
+            DateFormat in = new SimpleDateFormat("hh:mm:ssa");
+            DateFormat out = new SimpleDateFormat("HH:mm:ss");
+            Date date = in.parse(s);
+            return out.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String input = "07:05:45PM";
+        System.out.println(TimeConversion(input));
+    }
+}
