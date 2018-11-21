85783861b8dc6c2b501c0a97da46b8f3bbf42130
Simonas D <dranxman@gmail.com>
2018-11-21 21:03:17 +0200

    #grading students algorithm solution

--- src/com/company/Algorithms/GradingStudents.java
+++ src/com/company/Algorithms/GradingStudents.java
@@ -1,4 +1,23 @@
 package com.company.Algorithms;
 
 public class GradingStudents {
+    static int[] gradingStudents(int[] grades) {
+        int[] calculatedGrades = new int[grades.length];
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] < 38 || grades[i] % 5 < 3) {
+                calculatedGrades[i] = grades[i];
+            } else {
+                calculatedGrades[i] = grades[i] + (5 - (grades[i] % 5));
+            }
+        }
+        return calculatedGrades;
+    }
+
+    public static void main(String[] args) {
+        int[] grades = { 73, 67, 38, 33};
+        int[] calculateGrades = gradingStudents(grades);
+        for (int calculateGrade : calculateGrades) {
+            System.out.println(calculateGrade);
+        }
+    }
 }
