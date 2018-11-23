5e069d2d9839f821cb3943f716dca86b76ec1cb2
Simonas D <dranxman@gmail.com>
2018-11-23 18:48:39 +0200

    kangaroo problem

---
+++ src/com/company/Algorithms/Kangaroo.java
@@ -0,0 +1,15 @@
+package com.company.Algorithms;
+
+public class Kangaroo {
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        if (v1 > v2 &&(x1 - x2) % (v2 - v1) == 0 ) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kangaroo(0, 2, 5, 3));
+    }
+}
