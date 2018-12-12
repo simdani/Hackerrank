aa3991c384f27fc80082793fb0f06d9ab1d92a12
Simonas D <dranxman@gmail.com>
2018-12-12 21:56:56 +0200

    add initial cat and mouse

---
+++ src/com/company/Algorithms/ProblemSolving/Easy/CatsAndMouse.java
@@ -0,0 +1,11 @@
+package com.company.Algorithms.ProblemSolving.Easy;
+
+public class CatsAndMouse {
+    public String catAndMouse(int x, int y, int z) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
