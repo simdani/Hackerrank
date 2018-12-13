7a2df6d934295c42199ba806a59a979a0acf9e7d
Simonas D <dranxman@gmail.com>
2018-12-13 22:45:56 +0200

    Cat and mouse solution

--- src/com/company/Algorithms/ProblemSolving/Easy/CatsAndMouse.java
+++ src/com/company/Algorithms/ProblemSolving/Easy/CatsAndMouse.java
@@ -1,11 +1,19 @@
 package com.company.Algorithms.ProblemSolving.Easy;
 
 public class CatsAndMouse {
-    public String catAndMouse(int x, int y, int z) {
-        return "";
+
+    static String catAndMouse(int x, int y, int z) {
+        if (Math.abs(x - z) < Math.abs(y - z)) {
+            return "Cat A";
+        } else if (Math.abs(x - z) > Math.abs(y - z)) {
+            return "Cat B";
+        } else {
+            return "Mouse C";
+        }
     }
 
     public static void main(String[] args) {
 
+        System.out.println(catAndMouse(1, 2, 3));
     }
 }
