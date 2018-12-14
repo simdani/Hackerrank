f61f182a4e3e38078b3e5a299090023a8a7ccdde
Simonas D <dranxman@gmail.com>
2018-12-14 21:59:08 +0200

    sfds

---
+++ src/com/company/Algorithms/ProblemSolving/Easy/FindDigits.java
@@ -0,0 +1,11 @@
+package com.company.Algorithms.ProblemSolving.Easy;
+
+public class FindDigits {
+    private static int findDigits(int n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
